bdaf115d1a39be8ee20dbe374252e5a80f0cc83f
3A532085 <Luxgen077@gmail.com>
2019-01-13 15:06:30 +0800

    建立預約及現場的資料表、Controller、Model、Factory

---
+++ app/Http/Controllers/WaitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Wait;
+use Illuminate\Http\Request;
+
+class WaitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Wait  $wait
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Wait $wait)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Wait  $wait
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Wait $wait)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Wait  $wait
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Wait $wait)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Wait  $wait
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Wait $wait)
+    {
+        //
+    }
+}
