e638a30afd1732fac696bcf3ac33e360e548cedc
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2019-01-14 01:27:21 +0800

    連結Reservation Model

--- app/User.php
+++ app/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Wait::class);
     }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
