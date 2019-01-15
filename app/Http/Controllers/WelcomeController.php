2bc8d2cc7239d0c164e827fb759092f5cc6d4a05
3A532085 <Luxgen077@gmail.com>
2019-01-16 00:56:43 +0800

    管理者登入後,導入至後台頁面(身份識別)

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
         return view('welcome');
+    }
+
+    public function type()
+
+    {
+
+        $type=Auth::user()->type;
+
+        if($type == 1){
+            return redirect('/admin');
+        }
+
+        else{
+
+            return redirect('/home');
+
+        }
+
     }
 }
