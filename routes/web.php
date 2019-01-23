8290a8c211620d6ea78e26abe2fc74fb28266000
3A532085 <Luxgen077@gmail.com>
2019-01-24 05:14:45 +0800

    訂位人數&日期、訂位時段、選擇桌位、確認訂位資料的相關路徑

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,25 @@ Route::patch('/wait/status/{id}', 'AllWaitController@status');
 Route::post('/wait/inline', 'AllwaitController@inline');
 Route::patch('/num/{id}', 'AdminController@num');
 Route::patch('/time/{id}', 'AdminController@time');
+Route::get('seat', ['as' => 'reservation.seat',   'uses' => 'ReservationController@seat']);
+Route::patch('/section1/{id}', 'ReservationController@seat');
+Route::post('/reservationok', 'ReservationController@show');
+
+Route::get('/stepone', 'ReservationController@stepone');
+Route::get('stepone', ['as' => 'stepone.stepone',   'uses' => 'ReservationController@stepone']);
+Route::post('/step2/{id}', 'ReservationController@time');
+Route::patch('/reservationcheck/{reservationcheck}', 'ReservationController@index2');
+Route::patch('/section2/{id}', 'ReservationController@section2');
+Route::patch('/reservationcheck2/{reservationcheck2}', 'ReservationController@index3');
+Route::patch('/section3/{id}', 'ReservationController@section3');
+Route::patch('/reservationcheck3/{reservationcheck3}', 'ReservationController@index4');
+
+Route::post('/stepnew/{id}', 'ReservationController@timenew');
+Route::patch('/sectionnew/{id}', 'ReservationController@sectionnew');
+Route::patch('/sectionnew2/{id}', 'ReservationController@sectionnew2');
+Route::patch('/sectionnew3/{id}', 'ReservationController@sectionnew3');
+Route::patch('/reservationchecknew/{reservationchecknew}', 'ReservationController@index5');
+Route::patch('/reservationchecknew2/{reservationchecknew}', 'ReservationController@index6');
+Route::patch('/reservationchecknew3/{reservationchecknew}', 'ReservationController@index7');
+
+
